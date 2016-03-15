4f6f23cc1477185467af8641a8f8500cc91e4e45
shaun <virusest@126.com>
2016-03-15 16:55:00 +0800

    update

--- roles/mariadb/templates/hauser.sql
+++ roles/mariadb/templates/hauser.sql
@@ -1,6 +1,6 @@
 grant all on *.* to root@'%' identified by '{{ mysql_root_db_pass }}' with grant option;
-insert into mysql.user (Host,User) values ('{{ controller1_ipaddr }}','haproxy');
-insert into mysql.user (Host,User) values ('{{ controller2_ipaddr }}','haproxy');
-insert into mysql.user (Host,User) values ('{{ controller3_ipaddr }}','haproxy');
+insert into mysql.user (Host,User) values ("{{ groups[['controller'][0]][0] }}",'haproxy');
+insert into mysql.user (Host,User) values ("{{ groups[['controller'][0]][1] }}",'haproxy');
+insert into mysql.user (Host,User) values ("{{ groups[['controller'][0]][2] }}",'haproxy');
 flush privileges;
 
